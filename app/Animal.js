c278cfd4533b549dfe4fe8fe1f4954ca26a83f3c
idem <victoridongesit@gmail.com>
2017-02-13 14:43:32 +0100

    Cleaned up folders

--- app/Animal.js
+++ app/Animal.js
@@ -5,6 +5,8 @@ module.exports = function(name) {
 		this.sound = 'bark!';
 	}
 	this.name = this.name.toLowerCase();
+
+	// Assign sound an domestication based on name
 	switch(this.name){
 		case 'dog':
 			this.domestic = true;
@@ -30,6 +32,8 @@ module.exports = function(name) {
 			this.domestic = 'unknown';
 			this.sound = 'unknown';
 	}
+
+	// Make it talk
 	this.talk = function(){
 		return this.sound.toUpperCase();
 	}
