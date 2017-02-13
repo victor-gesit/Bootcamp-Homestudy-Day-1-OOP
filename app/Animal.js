d18942313c474318446666b67c25c3d50d4aa16c
idem <victoridongesit@gmail.com>
2017-02-13 14:30:52 +0100

    11 Animal spec tests passes successfully in node

--- app/Animal.js
+++ app/Animal.js
@@ -1,5 +1,37 @@
-module.exports = function() {
+module.exports = function(name) {
+	this.name = name;
+	if(name === undefined){
+		this.name = 'dog';
+		this.sound = 'bark!';
+	}
+	this.name = this.name.toLowerCase();
+	switch(this.name){
+		case 'dog':
+			this.domestic = true;
+			this.sound = 'Bark!';
+			break;
+		case 'cat':
+			this.domestic = true;
+			this.sound = 'Meow!';
+			break;
+		case 'cow':
+			this.domestic = true;
+			this.sound = 'Moow!'
+			break;
+		case 'lion':
+			this.domestic = false;
+			this.sound = 'Roar!';
+			break;
+		case 'monkey':
+			this.domestic = false;
+			this.sound = 'Whoop!';
+			break;
+		default:
+			this.domestic = 'unknown';
+			this.sound = 'unknown';
+	}
+	this.talk = function(){
+		return this.sound.toUpperCase();
+	}
 
-}
-
-//export {Animal};
+};
--- spec/AnimalSpec.js
+++ spec/AnimalSpec.js
@@ -7,7 +7,7 @@ const Animal = require('../app/Animal.js');
 	describe("Animal Class: Create an animal, make it talk", function(){
 		it("The animal should be of type 'object' and an instance of the animal Class", function(){
 			const cat = new Animal('cat');
-			expect(typeof cat).toEqual(typeof 22);
+			expect(typeof cat).toEqual(typeof {});
 			expect(cat instanceof Animal).toBeTruthy();
 		});
 		
@@ -18,31 +18,31 @@ const Animal = require('../app/Animal.js');
 		
 		it("should bark it it's a dog", function(){
 			const animal = new Animal('dog');
-				expect(animal.talk().toUpper()).toEqual('BARK!');
+				expect(animal.talk()).toEqual('BARK!');
 		});
 		it("should meow if it's a cat", function(){
 			const animal = new Animal('cat');
-			expect(animal.talk().toUpper()).toEqual('MEOW!');
+			expect(animal.talk()).toEqual('MEOW!');
 		});
 		it("Should moow if it's a cow", function(){
 			const animal = new Animal('cow');
-			expect(animal.talk().toUpper()).toEqual('MOOW!');
+			expect(animal.talk()).toEqual('MOOW!');
 		});
 		it("Should roar if it's a lion", function(){
-			const animal = new Animal('dog');
-			expect(animal.talk().toUpper()).toEqual('ROAR!');
+			const animal = new Animal('lion');
+			expect(animal.talk()).toEqual('ROAR!');
 		});
 		it("Should whoop if it's a monkey", function(){
-			const animal = new Animal('dog');
-			expect(animal.talk().toUpper()).toEqual('WHOOP!');
+			const animal = new Animal('monkey');
+			expect(animal.talk()).toEqual('WHOOP!');
 		});
 	})
 
 	// Overloading
-	describe("My favorite animal is a dog. If you don't specify, I'll make you a dog", function(){
+	describe("My favorite animal is a dog. If you don't specify, I'll make you a dog. It ", function(){
 		const animal = new Animal();
 		it('Should bark', function(){
-			expect(animal.talk().toUpper()).toEqual('BARK!');
+			expect(animal.talk()).toEqual('BARK!');
 		});
 		it('Should be domestic', function(){
 			expect(animal.domestic).toEqual(true);
